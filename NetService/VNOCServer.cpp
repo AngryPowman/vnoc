8963ec4ccc5593f8126b9a1fa76805764acbb984
AngryPowman <AngryPowman@36a5fca6-8feb-4701-ac75-490187a78786>
2012-02-09 16:57:30 +0000

    git-svn-id: http://subversion.assembla.com/svn/vnoc/trunk@20 36a5fca6-8feb-4701-ac75-490187a78786

--- NetService/VNOCServer.cpp
+++ NetService/VNOCServer.cpp
@@ -6,7 +6,7 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-#include "NetService.h"
+#include "src/NetService.h"
 #include <iostream>
 using namespace std;
 
