b9fb67ccb45bd7f1fa87322cdbe31df6e2e69870
AngryPowman <AngryPowman@36a5fca6-8feb-4701-ac75-490187a78786>
2012-02-09 15:32:36 +0000

    git-svn-id: http://subversion.assembla.com/svn/vnoc/trunk@17 36a5fca6-8feb-4701-ac75-490187a78786

---
+++ NetService/epoll_sample.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : epoll_server.cpp
+// Author      : AngryPowman
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include "NetService.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	NetService net;
+	net.start(12345);
+
+	return 0;
+}
