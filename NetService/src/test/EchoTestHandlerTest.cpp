c125505d3c15efb6c35eec653cfa32f915f2d801
longda <longda@36a5fca6-8feb-4701-ac75-490187a78786>
2012-04-12 05:54:26 +0000

    re #12
    git-svn-id: http://subversion.assembla.com/svn/vnoc/trunk@87 36a5fca6-8feb-4701-ac75-490187a78786

--- NetService/src/test/EchoTestHandlerTest.cpp
+++ NetService/src/test/EchoTestHandlerTest.cpp
@@ -5,17 +5,33 @@
 #include "EchoTestHandler.hpp"
 class EchoTestHandlerTest : public CppUnit::TestFixture
 {
-	CPPUNIT_TEST_SUITE( EchoTestHandlerTest );
-	CPPUNIT_TEST( SimpleTest );
-	CPPUNIT_TEST_SUITE_END();
+    CPPUNIT_TEST_SUITE( EchoTestHandlerTest );
+    CPPUNIT_TEST( simpleTest );
+    CPPUNIT_TEST( testSpecialChar );
+    CPPUNIT_TEST_SUITE_END();
+    Connection *conn_;
 public:
-	void SimpleTest()
-	{
-		Connection *conn = new Connection;
-		EchoTestHandler<Connection> handler(conn);
-		CPPUNIT_ASSERT(true);
-		delete conn;
-	}
+    void setUp()
+    {
+        conn_ = new Connection;
+    }
+    void tearDown()
+    {
+        delete conn_;
+    }
+public:
+    void simpleTest()
+    {
+        EchoTestHandler<Connection> handler(conn_);
+        CPPUNIT_ASSERT(true);
+    }
+    void testSpecialChar()
+    {
+        EchoTestHandler<Connection> handler(conn_);
+        CPPUNIT_ASSERT(true);
+    }
 };
 CPPUNIT_TEST_SUITE_REGISTRATION ( EchoTestHandlerTest );
+CPPUNIT_TEST_SUITE_NAMED_REGISTRATION(EchoTestHandlerTest, "EchoTestHandlerTest");
+
 #endif /*ECHOTESTHANDLERTEST_H*/
