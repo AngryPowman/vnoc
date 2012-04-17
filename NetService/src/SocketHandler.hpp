6ba726631098f78226b10143557dff9b1e44514c
longda <longda@36a5fca6-8feb-4701-ac75-490187a78786>
2012-04-17 01:23:11 +0000

    git-svn-id: http://subversion.assembla.com/svn/vnoc/trunk@96 36a5fca6-8feb-4701-ac75-490187a78786

--- NetService/src/SocketHandler.hpp
+++ NetService/src/SocketHandler.hpp
@@ -5,8 +5,6 @@ class SocketHandler
 {
 public:
 	virtual void start()=0;
-    virtual void ReadsomeHandler(const asio::error_code& error, size_t bytes_transferred)=0;    
-	virtual void HandleWrite(const asio::error_code& error)=0;
 };
 class SocketHandlerFactory
 {
