8e711b4d4d6c97cbeb22e298094547e8f664e739
boxsnake <boxsnake@36a5fca6-8feb-4701-ac75-490187a78786>
2012-04-26 20:40:11 +0000

    做完了frameset框架类，并加入了调试，晚上将这个上传到http://vnoc.atwebpages.com，请大家积极测试。 另外改了reset.php的代码，让表文件可以按照模块重建，避免数据误操作丢失。 下面透露一下下一个类：是几何图形类，敬请期待了。。。这个做完就开始做VNOC主界面了。谢谢大家一贯对VNOC的支持。
    git-svn-id: http://subversion.assembla.com/svn/vnoc/trunk@115 36a5fca6-8feb-4701-ac75-490187a78786

--- webClient/less_test/reset.php
+++ webClient/less_test/reset.php
@@ -1,3 +1,12 @@
 <?php
-	file_put_contents("locations.csv", "Info,frame_pos,window_pos_1,window_pos_2\n");
+	switch($_GET['section']){
+		case "locations":
+			file_put_contents("locations.csv", "Info,frame_pos,window_pos_1,window_pos_2\n");
+			break;
+		case "frameset":
+			file_put_contents("frameset.csv", "Info,report\n");
+			break;
+		default:
+			// 枚举终止;
+	}
 ?>
