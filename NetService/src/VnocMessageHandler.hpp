21d79f6ab9586a77c431336bcb2ae401605dd4e9
longda <longda@36a5fca6-8feb-4701-ac75-490187a78786>
2012-06-07 08:22:08 +0000

    refactor VnocMessageHandler
    git-svn-id: http://subversion.assembla.com/svn/vnoc/trunk@230 36a5fca6-8feb-4701-ac75-490187a78786

--- NetService/src/VnocMessageHandler.hpp
+++ NetService/src/VnocMessageHandler.hpp
@@ -5,118 +5,27 @@
 
 #include "AsioTcpConnection.hpp"
 #include "SocketHandler.hpp"
-#include "../../Message/MessageParser.h"
-#include "../../Message/PackMessage.h"
+#include "../../Message/MsgDef.h"
 #include <ezlogger_headers.hpp>
 template <typename ConnectionT>
 class VnocMessageHandler : public SocketHandler
 {
 public:
     VnocMessageHandler(ConnectionT *connection):connection_(connection){}
-    virtual void start()
-    {
-        connection_->recv(headerData_, sizeof(headerData_), 
-            std::bind(&VnocMessageHandler::ReadHeadHandler, this,
-          std::placeholders::_1,
-          std::placeholders::_2));
-    }
+    //start handler the message on the connection
+    virtual void start();
+
+    //post a read operation to wait for message header.
+    void readHeader();
 
 private:
-    void ReadHeadHandler(const asio::error_code& error, size_t bytes_transferred)
-    {
-        if (error) {
-            delete this;
-            return;
-        }
-        if (bytes_transferred != HEAD_LEN) {
-            EZLOGGERVLSTREAM(axter::log_often)<<"head lenth miss match\n";
-            connection_->recv(headerData_, sizeof(headerData_), 
-                std::bind(&VnocMessageHandler::ReadHeadHandler, this,
-                std::placeholders::_1,
-                std::placeholders::_2));
-            return;
-        }
-        size_t package_len = htonl(*(int *)(headerData_+4));
-        if (package_len <= HEAD_LEN) {
-            EZLOGGERVLSTREAM(axter::log_often)<<"package_len <= HEAD_LEN\n";
-            connection_->recv(headerData_, sizeof(headerData_), 
-                std::bind(&VnocMessageHandler::ReadHeadHandler, this,
-                std::placeholders::_1,
-                std::placeholders::_2));
-            return;
-        } 
-        char *messageBuffer ( new char[package_len]);
-        memcpy(messageBuffer, headerData_, HEAD_LEN);
-        connection_->recv(&messageBuffer[HEAD_LEN], package_len - HEAD_LEN, 
-                std::bind(&VnocMessageHandler::ReadBodyHandler, this, messageBuffer,
-                std::placeholders::_1,
-                std::placeholders::_2));
+    void ReadHeaderHandler(const asio::error_code& error, size_t bytes_transferred);
+	void ReadBodyHandler(char *messageBuffer, const asio::error_code& error, size_t bytes_transferred);
+    void SendHandler(char* buffer, const asio::error_code& error, size_t bytes_transferred);
 
-    }
-
-	void ReadBodyHandler(char *messageBuffer, const asio::error_code& error, size_t bytes_transferred){
-        std::unique_ptr<char[]> safe_buf(messageBuffer);
-        if (error) {
-            delete this;
-            return;
-        }
-        //process the message here
-        EZLOGGERVLSTREAM(axter::log_rarely)<<"receve message body\n";
-        CMessageParser parser;
-        std::unique_ptr<CMessage> msg(parser.Parse((byte*)safe_buf.get(), bytes_transferred + HEAD_LEN));
-        int ret=0;
-        if (msg != NULL){
-            int type = msg->GetMessageType();
-
-            switch (type){
-            case MSG_RVC_TYPE:
-                ret = HandlerRVCMessage(dynamic_cast<MSG_RVC*>(msg.get()));
-                break;
-            default:
-                ret = 0;
-            };
-        }
-
-        if (ret == 0){
-            //message is not handled, we need post a read opeartion
-            connection_->recv(headerData_, sizeof(headerData_), 
-                std::bind(&VnocMessageHandler::ReadHeadHandler, this,
-                std::placeholders::_1,
-                std::placeholders::_2));
-        }
-	}
-
-    void SendHandler(char* buffer, const asio::error_code& error, size_t bytes_transferred)
-    {
-        delete []buffer;
-        if (!error){
-            connection_->recv(headerData_, sizeof(headerData_), 
-                std::bind(&VnocMessageHandler::ReadHeadHandler, this,
-                std::placeholders::_1,
-                std::placeholders::_2));
-        }
-    }
-
-    int HandlerRVCMessage(MSG_RVC *rvcMessage)
-    {
-        MSG_AVC avcMessage;
-        avcMessage.SetCaptchaType(0);
-        byte captcha[] = {0};
-        avcMessage.SetCaptcha(captcha,sizeof(captcha));
-        avcMessage.SetLoginTag(1);
-        PackMessage packer;
-        size_t avcLen = packer.GetMessageLen(&avcMessage);
-        char *avcPack = new char[avcLen];
-        packer.Pack(&avcMessage, (byte*)avcPack, avcLen);
-        connection_->send(avcPack, avcLen,
-                std::bind(&VnocMessageHandler::SendHandler, this, avcPack,
-                std::placeholders::_1,
-                std::placeholders::_2));
-
-        return 1;
-    }
-    const static size_t HEAD_LEN = 30;
-    char headerData_[HEAD_LEN];
+    int HandleRVCMessage(MSG_RVC *rvcMessage);
+    const static size_t HEADER_LEN = 30;
+    char headerData_[HEADER_LEN];
     ConnectionT *connection_;
 };
 class VnocMessageHandlerFactory: public SocketHandlerFactory
@@ -127,4 +36,5 @@ public:
         return new VnocMessageHandler<AsioTcpConnection>(connection);
     }
 };
+#include "VnocMessageHandler.inl"
 #endif //VNOCMESSAGEHANDLER_HPP
---
+++ NetService/src/VnocMessageHandler.inl
@@ -0,0 +1,103 @@
+
+#include "../../Message/MessageParser.h"
+#include "../../Message/PackMessage.h"
+#include <ezlogger_headers.hpp>
+
+template <typename ConnectionT>
+void VnocMessageHandler<ConnectionT>::readHeader(){
+    connection_->recv(headerData_, sizeof(headerData_), 
+        std::bind(&VnocMessageHandler::ReadHeaderHandler, this,
+        std::placeholders::_1,
+        std::placeholders::_2));
+}
+
+template <typename ConnectionT>
+void VnocMessageHandler<ConnectionT>::start()
+{
+    readHeader();
+}
+
+template <typename ConnectionT>
+void VnocMessageHandler<ConnectionT>::ReadHeaderHandler(const asio::error_code& error, size_t bytes_transferred)
+{
+    if (error) {
+        delete this;
+        return;
+    }
+    if (bytes_transferred != HEADER_LEN) {
+        EZLOGGERVLSTREAM(axter::log_often)<<"header lenth miss match\n";
+        readHeader();
+        return;
+    }
+    size_t package_len = htonl(*(int *)(headerData_+4));
+    if (package_len <= HEADER_LEN) {
+        EZLOGGERVLSTREAM(axter::log_often)<<"package_len <= HEAD_LEN\n";
+        readHeader();
+        return;
+    } 
+    char *messageBuffer ( new char[package_len]);
+    memcpy(messageBuffer, headerData_, HEADER_LEN);
+    connection_->recv(&messageBuffer[HEADER_LEN], package_len - HEADER_LEN, 
+        std::bind(&VnocMessageHandler::ReadBodyHandler, this, messageBuffer,
+        std::placeholders::_1,
+        std::placeholders::_2));
+}
+
+template <typename ConnectionT>
+void VnocMessageHandler<ConnectionT>::ReadBodyHandler(char *messageBuffer, const asio::error_code& error, size_t bytes_transferred){
+    std::unique_ptr<char[]> safe_buf(messageBuffer);
+    if (error) {
+        delete this;
+        return;
+    }
+    //process the message here
+    EZLOGGERVLSTREAM(axter::log_rarely)<<"receve message body\n";
+    CMessageParser parser;
+    std::unique_ptr<CMessage> msg(parser.Parse((byte*)safe_buf.get(), bytes_transferred + HEADER_LEN));
+    int ret=0;
+    if (msg != NULL){
+        int type = msg->GetMessageType();
+
+        switch (type){
+        case MSG_RVC_TYPE:
+            ret = HandleRVCMessage(dynamic_cast<MSG_RVC*>(msg.get()));
+            break;
+        default:
+            ret = 0;
+        };
+    }
+
+    if (ret == 0){
+        //message is not handled, we need post a read opeartion
+        readHeader();
+    }
+}
+
+template <typename ConnectionT>
+void VnocMessageHandler<ConnectionT>::SendHandler(char* buffer, const asio::error_code& error, size_t bytes_transferred)
+{
+    delete []buffer;
+    if (!error){
+        readHeader();
+    }
+}
+
+template <typename ConnectionT>
+int VnocMessageHandler<ConnectionT>::HandleRVCMessage(MSG_RVC *rvcMessage)
+{
+    MSG_AVC avcMessage;
+    avcMessage.SetCaptchaType(0);
+    byte captcha[] = {0};
+    avcMessage.SetCaptcha(captcha,sizeof(captcha));
+    avcMessage.SetLoginTag(1);
+    PackMessage packer;
+    size_t avcLen = packer.GetMessageLen(&avcMessage);
+    char *avcPack = new char[avcLen];
+    packer.Pack(&avcMessage, (byte*)avcPack, avcLen);
+    connection_->send(avcPack, avcLen,
+        std::bind(&VnocMessageHandler::SendHandler, this, avcPack,
+        std::placeholders::_1,
+        std::placeholders::_2));
+
+    return 1;
+}
