3363b80ac1c187368178773c284e7cbb0c03d640
iceable <iceable@36a5fca6-8feb-4701-ac75-490187a78786>
2012-06-14 13:54:16 +0000

    用户模块的登陆验证
    git-svn-id: http://subversion.assembla.com/svn/vnoc/trunk@257 36a5fca6-8feb-4701-ac75-490187a78786

--- NetService/src/test/testRunner.cpp
+++ NetService/src/test/testRunner.cpp
@@ -1,11 +1,19 @@
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/ui/text/TestRunner.h>
 
+#include "..\testUserManage.hpp"
+#include <iostream>
+using namespace std;
+
 int main( int argc, char **argv)
 {
   CppUnit::TextUi::TestRunner runner;
   CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
   runner.addTest( registry.makeTest() );
   bool wasSuccessful = runner.run( "", false );
+
+  	testUserManage test;
+	cout << "AuthenticateTest Return : "<<test.AuthenticateTest()<<endl;
+
   return wasSuccessful;
 }
