2fe0f5be14dc1255b4f7fbb90308b0434b2e7779
real.longda <real.longda@gmail.com>
2012-06-23 02:26:57 +0800

    use po to read config file.

--- NetService/src/Config.hpp
+++ NetService/src/Config.hpp
@@ -1,9 +1,14 @@
 #pragma once
 #include <string>
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
 class Config
 {
 public:
-    void Initialize();
+    bool Initialize(std::string config_file);
     std::string getValue(std::string key);
     static Config* getInstance();
+private:
+    po::variables_map vm_;
+    static Config instance_;
 };
