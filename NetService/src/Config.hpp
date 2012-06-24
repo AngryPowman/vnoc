3d16ccfcae68857cfef3d35e226b7b5fce5f88ee
real.longda <real.longda@gmail.com>
2012-06-24 16:23:52 +0800

    add getStringValue

--- NetService/src/Config.hpp
+++ NetService/src/Config.hpp
@@ -6,13 +6,13 @@ class Config
 {
 public:
     bool Initialize(std::string config_file);
-    std::string getStringValue(std::string key);
     struct ConfigValue{
         ConfigValue(std::string value):m_value(value){}
         operator int(){return atoi(m_value.c_str());}
         operator std::string(){return m_value;}
         std::string m_value;
     };
+    std::string getStringValue(std::string key);
     ConfigValue getValue(std::string key){return ConfigValue((getStringValue((key))));}
     static Config* getInstance();
 private:
