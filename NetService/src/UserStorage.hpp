ba65678b06ae93a72900c48289db5b83dfd30c94
able <645989637@qq.com>
2012-07-07 08:38:10 +0800

    增加userstorage的文件读取用户信息

--- NetService/src/UserStorage.hpp
+++ NetService/src/UserStorage.hpp
@@ -1,7 +1,7 @@
 #pragma once
-
+#include <fstream>
 #include "UserInfo.hpp"
-
+using namespace std;
 class UserStorage
 {
 public:
@@ -9,29 +9,66 @@ public:
 	~UserStorage();
 	//返回密码的长度
 	bool IfUserExist(const char* pUser);
-	int GetPassword(const char* pUser, char* pPassWordBuff, long cbBuff);
-	int GetUserInfo(const char* pUser, userinfo* pUserInfo);
+	bool GetPassword(const char* pUser, char* pPassWordBuff, long cbBuff);
+	bool GetUserInfo(const char* pUser, userinfo* pUserInfo);
 private:
-
+	int nUserCount;
+	userinfo *pUserTable;
 };
 
 UserStorage::UserStorage()
 {
+	fstream file;
+	int nSize;
+	file.open("../userinfo", ios::binary|ios::in,0);
+	file.seekg(0, ios::end);
+	nSize = file.tellg();
+	nUserCount = nSize/148;
+	pUserTable = new userinfo[nUserCount];
+	file.seekg(0, ios::beg);
+	file.read((char *)pUserTable, nSize);
+	file.close();
 }
 
 UserStorage::~UserStorage()
 {
+	delete pUserTable;
 }
 
 bool UserStorage::IfUserExist(const char* pUser)
 {
-	return true;
+	for (int i = 0; i < nUserCount; i++)
+	{
+		if ( strcmp(pUserTable[i].strUser, pUser) == 0)
+		{
+			return true;
+		}
+	}
+	return false;
 }
-int UserStorage::GetPassword(const char* pUser, char* pPassWordBuff, long cbBuff)
+bool UserStorage::GetPassword(const char* pUser, char* pPassWordBuff, long cbBuff)
 {
-	return 0;
+	for (int i = 0; i < nUserCount; i++)
+	{
+		if ( strcmp(pUserTable[i].strUser, pUser) == 0)
+		{
+			memcpy(pPassWordBuff, pUserTable[i].passwordData, cbBuff);
+			return true;
+		}
+	}
+	return false;
 }
-int UserStorage::GetUserInfo(const char* pUser, userinfo* pUserInfo)
+bool UserStorage::GetUserInfo(const char* pUser, userinfo* pUserInfo)
 {
-	return 0;
+	userinfo *p = pUserTable;
+	for (int i = 0; i < nUserCount; i++)
+	{
+		if ( strcmp(pUserTable[i].strUser, pUser) == 0)
+		{
+			memcpy(pUserInfo, p, sizeof(userinfo));
+			return true;
+		}
+		++p;
+	}
+	return false;
 }
