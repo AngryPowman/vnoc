d4a972579cb878b4a2c657ea0b8db74b649b0e56
epiciskandar <epiciskandar@gmail.com>
2012-08-29 20:46:58 +0800

    增加资源文件的拷贝操作，保证编译后可直接运行 BUGFIX=0 ISSUE=0

--- VNOC/VNOCC/postDispatch.bat
+++ VNOC/VNOCC/postDispatch.bat
@@ -3,6 +3,8 @@ setlocal EnableDelayedExpansion
 
 @xcopy /f/c/y "%1/*.dll" "../%1" > nul
 @xcopy /f/c/y "config/*.xml" "../%1" > nul
+@mkdir "../%1/WinRes" > nul
+@xcopy /f/c/y "res/*.xml" "../%1/WinRes" > nul
 
 :gEnd
 @echo "Post Dispatch OK!"
