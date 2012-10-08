6fe5df396b7308741d39483c00341aa52278a3c8
epiciskandar <epiciskandar@gmail.com>
2012-10-08 16:17:52 +0800

    解决vldmemleakcheck配置下无法工作的问题

--- VNOC/VNOC/postDispatch.bat
+++ VNOC/VNOC/postDispatch.bat
@@ -2,6 +2,7 @@
 setlocal EnableDelayedExpansion
 
 @xcopy /f/c/y "%1/*.dll" "../%1" > nul
+@xcopy /f/c/y "%1/*.manifest" "../%1" > nul
 @xcopy /f/c/y "config/*.xml" "../%1" > nul
 @mkdir "../%1/WinRes" > nul
 @xcopy /f/c/y "res/*.xml" "../%1/WinRes" > nul
