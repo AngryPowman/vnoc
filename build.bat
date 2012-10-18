da2aa0837f12d2aeb770544fec0173762c62d8e7
epiciskandar <epiciskandar@gmail.com>
2012-10-19 02:05:33 +0800

    更改编译脚本。将客户端输出到Artifacts中

--- build.bat
+++ build.bat
@@ -1,1 +1,14 @@
-"%VS10PATH%/devenv.com" ./VNOC/VNOC.sln /build
+@echo off
+
+@set ClientDestDir=.\Artifacts\Client\
+@set ClientSrcDir=.\VNOC\Release
+
+@echo ================	Build	=================
+"%VS10PATH%\devenv.com" .\VNOC\VNOC.sln \build Release
+
+@echo ================	Digital Sigin	================
+
+@echo ================	Copy Artifacts	================
+@mkdir "%ClientDestDir%"
+@xcopy "%ClientSrcDir%" "%ClientDestDir%" /e/c/y
+@echo ================	End~~	=================
