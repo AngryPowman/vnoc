7f3e259cf61a9a53bbb162b58f56e85739f28bda
epiciskandar <epiciskandar@gmail.com>
2012-10-19 02:35:43 +0800

    编译脚本中添加server编译 修改server项目依赖

--- build.bat
+++ build.bat
@@ -1,14 +1,40 @@
 @echo off
 
+@set ServerDestDir=.\Artifacts\Server\
+@set ServerSrcDir=.\NetService\Release
+
 @set ClientDestDir=.\Artifacts\Client\
 @set ClientSrcDir=.\VNOC\Release
 
-@echo ================	Build	=================
-"%VS10PATH%\devenv.com" .\VNOC\VNOC.sln \build Release
+@echo .
+@echo .
+@echo .
+@echo ================	Build Server	=================
+"%VS11PATH%\devenv.com" .\NetService\server.sln /build "Release"
 
+@echo .
+@echo .
+@echo .
+@echo ================	Build Client	=================
+"%VS10PATH%\devenv.com" .\VNOC\VNOC.sln /build "Release|Win32"
+
+@echo .
+@echo .
+@echo .
 @echo ================	Digital Sigin	================
 
+@echo .
+@echo .
+@echo .
 @echo ================	Copy Artifacts	================
+@mkdir "%ServerDestDir%"
+@xcopy "%ServerSrcDir%" "%ServerDestDir%" /e/c/y
+
 @mkdir "%ClientDestDir%"
 @xcopy "%ClientSrcDir%" "%ClientDestDir%" /e/c/y
+
+@echo .
+@echo .
+@echo .
 @echo ================	End~~	=================
+@exit /b 0
