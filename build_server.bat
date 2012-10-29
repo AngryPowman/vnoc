48c5766823841a1149e5065088d444d38b18cbc5
real.longda <real.longda@gmail.com>
2012-10-29 13:34:12 +0800

    add build script for server

---
+++ build_server.bat
@@ -0,0 +1,32 @@
+@echo off
+
+@set ServerDestDir=.\Artifacts\Server\
+@set ServerSrcDir=.\NetService\Release
+
+@set ClientDestDir=.\Artifacts\Client\
+@set ClientSrcDir=.\VNOC\Release
+
+@echo .
+@echo .
+@echo .
+@echo ================	Build Server	=================
+"%VS11PATH%\devenv.com" .\NetService\server.sln /rebuild "Release"
+
+@echo .
+@echo .
+@echo .
+@echo ================	Unit Test	=================
+"%ServerDestDir%/testRunner.exe"
+
+@echo .
+@echo .
+@echo .
+@echo ================	Copy Artifacts	================
+@mkdir "%ServerDestDir%"
+@xcopy "%ServerSrcDir%" "%ServerDestDir%" /e/c/y
+
+@echo .
+@echo .
+@echo .
+@echo ================	End~~	=================
+@exit /b 0
