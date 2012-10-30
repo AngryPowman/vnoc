2c5c1e1ada481e30183f8b3953fa73f5e8998222
reallongda <tengda@brightbottle.peking.corp.yahoo.com>
2012-10-29 21:57:26 -0700

    remove build server from build.bat

--- build.bat
+++ build.bat
@@ -6,15 +6,6 @@
 @set ClientDestDir=.\Artifacts\Client\
 @set ClientSrcDir=.\VNOC\Release
 
-@echo .
-@echo .
-@echo .
-@echo ================	Build Server	=================
-"%VS11PATH%\devenv.com" .\NetService\server.sln /rebuild "Release"
-
-@echo .
-@echo .
-@echo .
 @echo ================	Build Client	=================
 "%VS10PATH%\devenv.com" .\VNOC\VNOC.sln /rebuild "Release|Win32"
 
@@ -27,8 +18,6 @@
 @echo .
 @echo .
 @echo ================	Copy Artifacts	================
-@mkdir "%ServerDestDir%"
-@xcopy "%ServerSrcDir%" "%ServerDestDir%" /e/c/y
 
 @mkdir "%ClientDestDir%"
 @xcopy "%ClientSrcDir%" "%ClientDestDir%" /e/c/y
