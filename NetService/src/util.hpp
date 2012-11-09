605fc756cc732ef3b77d960402d04c442a9a78b3
real.longda <real.longda@gmail.com>
2012-11-10 02:02:01 +0800

    rewrite smart_buf with shared_ptr

--- NetService/src/util.hpp
+++ NetService/src/util.hpp
@@ -2,32 +2,39 @@
 #ifndef UTIL_H_
 #define UTIL_H_
 #include <stdlib.h>
+#include <memory>
+using namespace std;
 
-class smart_buf{
+class smart_buf_imp{
 public:
-    smart_buf(char *raw){
+    smart_buf_imp(char *raw){
         this->buf_ = raw;
     }
-    smart_buf():buf_(NULL){};
-    ~smart_buf(){
+    smart_buf_imp():buf_(NULL){};
+    ~smart_buf_imp(){
         delete[] buf_;
     }
-    smart_buf(const smart_buf &other)
-    {
-        if (&other != this){
-            this->buf_ = other.buf_;
-            other.buf_ = NULL;
-        }
-    }
+
     operator char *(){
         return buf_;
     }
-    operator unsigned char *(){
-        return (unsigned char*)buf_;
-    }
+
 private:
     mutable char *buf_;
+    smart_buf_imp(const smart_buf_imp &other){}
+    smart_buf_imp &operator =(const smart_buf_imp &other){}
 };
+class smart_buf{
+public:
+    smart_buf(char *raw):imp_(new smart_buf_imp(raw)){}
 
-
+    operator char *(){
+        return (char *)(*imp_);
+    }
+    operator unsigned char *(){
+        return (unsigned char*)(char *)*this;
+    }
+private:
+    shared_ptr<smart_buf_imp> imp_;
+};
 #endif //UTIL_H_
