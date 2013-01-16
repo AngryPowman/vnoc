9b0a2bbac2d0e821770a797b1a01119bff8d3f19
zapline <278998871@qq.com>
2013-01-16 17:10:57 +0800

    * 修改ArrayData

--- NMessage/MsgDataValue/ArrayData.cpp
+++ NMessage/MsgDataValue/ArrayData.cpp
@@ -1,28 +1,44 @@
+#include "ArrayData.h"
 
-// #include "ArrayData.h"
-// 
-// template<class ValueType>
-// MsgStatus VNOC::Message::ArrayData<ValueType>::WriteList( IN const ArrayData<ValueType> ValueList )
-// {
-//     if (ValueList.Empty())
-//     {
-//         return MsgStatus_Err;
-//     }
-//     std::copy(ValueList.Begin(),ValueList.End(),m_ValueArr.begin());
-//     return  MsgStatus_Ok;
-// }
-// 
-// template<class ValueType>
-// MsgStatus VNOC::Message::ArrayData<ValueType>::ReadList( IN ArrayData<ValueType>& ValueList )
-// {
-//     if (m_ValueArr.empty())
-//     {
-//         return MsgStatus_Err;
-//     }
-//     auto Itr = m_ValueArr.begin();
-//     for (; Itr != m_ValueArr.end(); Itr++ )
-//     {
-//         ValueList.Push(*Itr);
-//     }
-//     return MsgStatus_Ok;
-// }
+namespace VNOC
+{
+namespace Message
+{
+
+ArrayData::ArrayData()
+{
+
+}
+
+ArrayData::~ArrayData()
+{
+
+}
+
+size_t ArrayData::Size()
+{
+    return m_arrValuePtr.size();
+}
+
+bool ArrayData::Empty()
+{
+    return m_arrValuePtr.empty();
+}
+
+std::vector<MsgDataValue*>::const_iterator ArrayData::Begin() const
+{
+    return m_arrValuePtr.begin();
+}
+
+std::vector<MsgDataValue*>::const_iterator ArrayData::End() const
+{
+    return m_arrValuePtr.end();
+}
+
+void ArrayData::Push( MsgDataValue* pValue )
+{
+    return m_arrValuePtr.push_back(pValue);
+}
+
+}// namespace Message
+}// namespace VNOC
