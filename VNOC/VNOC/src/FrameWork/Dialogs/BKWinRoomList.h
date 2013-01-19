d4c83525e2f4c64db63e4084d9d94271c998fc06
YeZi <645059545@qq.com>
2013-01-19 23:27:10 +0800

    整合了影子的代码 更新了UI

---
+++ VNOC/VNOC/src/FrameWork/Dialogs/BKWinRoomList.h
@@ -0,0 +1,61 @@
+#pragma once
+#include "../../BKWin/wtlhelper/whwindow.h"
+#include "../../net/INet.h"
+#include "../FrameBase.hpp"
+
+struct ColumnData
+{
+	ColumnData()
+	: strColumn1(_T("Column1")), strColumn2(_T("Column2"))
+	, strColumn3(_T("Column3")),  strColumn4(_T("Column4"))	
+	{
+	}
+
+	CString strColumn1, strColumn2, strColumn3, strColumn4;
+};
+
+struct ListItemData
+{
+	ListItemData()
+	: strItemData1("NULL"), strItemData2("NULL")
+	, strItemData3("NULL"), strItemData4("NULL")	
+	{
+	}
+
+	CString strItemData1, strItemData2, strItemData3, strItemData4;
+};
+
+
+class CRoomListWnd : public CBkDialogImpl<CRoomListWnd>
+	,public CFrameBase
+{
+public:
+	CRoomListWnd::CRoomListWnd() : CBkDialogImpl<CRoomListWnd>(BKDlg_RoomListWin),CFrameBase(module_RoomListWin)
+	{
+	}
+
+
+public:
+	void	OnBkBtnClose();
+	void	OnShowWnd(XMessage* pmsg);
+	LRESULT OnInitDialog(HWND hWnd, LPARAM lparam);
+	LRESULT OnListItemDblClick(int idRealWnd, LPNMHDR pnmh, BOOL& bHandled);
+	LRESULT OnShowRoomListResult(XMessage *pmsg);
+
+private:
+	CListViewCtrlEx m_wndListCtrl;
+
+private:
+	LRESULT	_ColumnInit();
+
+public:
+	BK_NOTIFY_MAP(IDC_RICHVIEW_WIN)
+		BK_NOTIFY_ID_COMMAND(DlgControl_RoomListWin_ImgBtn_Close,	OnBkBtnClose)
+	BK_NOTIFY_MAP_END()
+	
+	BEGIN_MSG_MAP_EX_DECLARE(CRoomListWnd)
+		Begin_XMessage(CRoomListWnd)
+		OnXMessage(XMessage_ShowRoomList,OnShowWnd)
+		OnXMessage(XMessage_ShowRoomList_Result, OnShowRoomListResult)
+	End_XMessage()
+};
