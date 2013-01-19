d086a386124bf0ef855cc09bf224935b82fd5467
Xosdy <xosdy.t@gmail.com>
2013-01-19 12:38:44 +0800

    修复注释乱码

--- VNOC/VNOC/src/Logic/room/IRoomMgr.h
+++ VNOC/VNOC/src/Logic/room/IRoomMgr.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 #include <list>
 #include "../../Base/IModule.h"
 #include "Room.h"
@@ -6,17 +6,17 @@
 
 
 typedef int RoomID;
-// �����б�
+// 房间列表
 typedef std::vector<RoomID> RoomList;
 
 interface IRoomMgr : public IModule
 {
 public:
 	STDMETHOD( Show(BOOL bShow=TRUE) = 0 );
-	// ��������
+	// 创建房间
 	STDMETHOD( CreateRoom(RoomID& id) = 0 );
 	STDMETHOD( GetRoom(RoomID id,CRoomBase* room) = 0 );
 	STDMETHOD( GetRoomList() = 0 );
-	// �رշ���
+	// 关闭房间
 	STDMETHOD( FreeRoom(RoomID id) = 0);
 };
