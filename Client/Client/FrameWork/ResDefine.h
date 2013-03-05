ef4d9d7f512eff2046f2f749794fb35dab9a2b24
Xosdy <xosdy.t@gmail.com>
2013-03-05 21:24:41 +0800

    恢复ResDefine.h控件分组

--- Client/Client/FrameWork/ResDefine.h
+++ Client/Client/FrameWork/ResDefine.h
@@ -10,17 +10,14 @@
 #define BKDlg_RoomListWin	_T("RoomListWin.xml")
 #define BKDlg_ClassroomWin   _T("ClassroomWin.xml")
 
-#define DlgControl_LoginWin_Edit_UserName	 10100
-#define DlgControl_LoginWin_Edit_PassWord	 10101
-#define DlgControl_RoomListWin_Edit_ClsId	 10111
+#define DlgControl_LoginWin_ImgBtn_Close        10001
+#define DlgControl_LoginWin_ImgBtn_Maximize     10002
+#define DlgControl_LoginWin_ImgBtn_Minimize     10003
+#define DlgControl_LoginWin_Edit_UserName       10100
+#define DlgControl_LoginWin_Edit_PassWord       10101
+#define DlgControl_LoginWin_Button_Login        10110
 
-#define DlgControl_LoginWin_ImgBtn_Minimize	 10003
-#define DlgControl_LoginWin_ImgBtn_Maximize	 10002
-#define DlgControl_LoginWin_ImgBtn_Close	 10001
-#define DlgControl_RoomListWin_ImgBtn_Close	 10001
-#define DlgControl_ClassroomWin_ImgBtn_Close 10001
+#define DlgControl_RoomListWin_ImgBtn_Close     10001
+#define DlgControl_RoomListWin_ListCtl_RoomList 10002
 
-#define DlgControl_LoginWin_Button_Login	 10110
-#define DlgControl_RoomlistWin_Button_Search 10004
-
-#define DlgControl_RoomListWin_Realwnd		 1001
+#define DlgControl_ClassroomWin_ImgBtn_Close    10001
